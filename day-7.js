a3b6ec69f4a547cb51fb79215ef4c6854c0f0afd
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-08-03 18:13:08 +0530

    Added DSA Day 7

---
+++ day-7.js
@@ -0,0 +1,20 @@
+//   loop
+
+// for(let i = 1; i<=100; i++){
+//   console.log(i);
+  
+// }
+
+
+// Entry control loop
+
+
+// for loop
+
+ let prompt =  require("prompt-sync")()
+
+let num = Number(prompt("enter a number: "));
+
+for(let i = 1; i<=num ; i++){
+  console.log("Hello World");
+}
