cc5d633883c52d7fc688bf79b6f5c602337f82f8
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-02 18:06:10 +0530

    Added day 22 and day 23 DSA

---
+++ day-22.js
@@ -0,0 +1,24 @@
+// //   string part 1
+
+
+
+// let name = "ravi"
+
+// console.log(typeof name);
+// console.log( name[3]);
+// console.log( name.length);
+
+
+
+
+
+// let name = "ravi"
+// console.log(name.slice(-3,-1));
+
+
+// let name = "ravi"
+// console.log(name.substring(3,1));
+
+
+   let name = "ravi"
+  console.log(name.concat("shankar" ,  "  tiwari"));  
---
+++ day-23.js
@@ -0,0 +1,12 @@
+// let prompt = require("prompt-sync")()
+
+// let str = prompt("enter any string:  ")
+
+// let ans = "";
+
+// for()
+
+
+
+
+  
