40e19137d57368d7cc94a9d9ba9bb6b742732d6c
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-03 22:43:18 +0530

    day-26---function

---
+++ day-26.js
@@ -0,0 +1,34 @@
+// function fun(){
+//   console.log("hello world");
+  
+// }
+
+// console.log();
+// console.log();
+// console.log();
+// fun()
+// console.log();
+// console.log();
+// fun()
+
+
+
+//        PARAMETER
+
+// function  fun(name , age) {
+//  console.log(`my name is ${name} and age is ${age}`);
+   
+// }
+
+// fun("ravi", 22)
+// fun("ali", 42)
+ 
+
+
+
+
+
+
+
+
+
