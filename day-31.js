19610bdf2b4da8ce129558eba389f60eead5a586
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-15 04:55:18 +0530

    ..

---
+++ day-31.js
@@ -0,0 +1,125 @@
+// // let set = new Set();
+
+// // set.add(10)
+// // set.add(10)
+// // set.add(10)
+// // set.add(10)
+// // set.add(10)
+// // set.add(10)
+// // set.add(10)
+
+// // console.log(set);
+
+
+
+
+
+
+// let set = new Set();
+
+// set.add(30)
+// set.add(30)
+// set.add(40)
+// set.add(50)
+// set.add(60)
+// set.add(70)
+// set.add(80)
+
+// console.log(set);
+// set.delete(50)
+// console.log(set);
+//  console.log(set.size );
+ 
+
+
+// let arr= [1,2,3,4,5,3,2,4,5,1,5]
+// let set = new Set()
+
+// for(let i=0; i<arr.length-1; i++){
+//   set.add(arr[i])
+// }
+
+// console.log(set);
+
+
+
+// let arr= [1,2,3,4,5,3,2,4,5,1,5]
+// let set = new Set(arr)
+
+
+
+// console.log(set);
+
+
+
+
+
+
+
+
+
+// let arr= [1,2,3,4,9,7,8,0,5,3,2,4,5,1,5]
+// let set = new Set();
+
+// for(let i=0; i<arr.length-1; i++){
+//   if(set.has(arr[i]))set.delete(arr[i])
+
+//     else set.add(arr[i])
+// }
+
+// console.log(set);
+
+
+
+
+
+
+
+
+// let arr = [1,2,3,21,3,43,2,6,4,5,3,2,5,4,1,3,35,5,6]
+
+// let set = new Set()
+
+// for(let i=0; i<arr.length; i++){
+//   set.add(arr[i])
+// }
+
+// console.log(set);
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,3,21,3,43,2,6,4,5,3,2,5,4,1,3,35,5,6]
+
+// let set = new Set(arr)
+
+
+// console.log(set);
+
+
+
+
+
+
+
+
+
+
+
+ let arr = [1,2,3,4,5,6,5,4,2,3,7,1,4,3,5,1,3,5,6,4]
+ let set = new Set()
+
+ for(let i=0; i<arr.length; i++){
+
+  if(set.has(arr[i]))set.delete(arr[i])
+    else(set.add(arr[i]))
+ }
+
+ console.log(set);
+ 
